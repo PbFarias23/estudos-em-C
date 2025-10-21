247127e4f5e9812be28b6e0f0fbaed67f15474b6
nome <email>
2025-10-21 15:48:30 -0300

    Aula07-exercicio3

--- Exercicios em C/Aula07.c
+++ Exercicios em C/Aula07.c
@@ -2,5 +2,25 @@
 
 int main (){
 
-    
-}
+    int dia;
+
+    printf("Digite o valor do dia!\n");
+    scanf("%d", &dia);
+
+    if (dia == 1)
+    {
+        printf("Domingo\n");
+    }   else if (dia == 2){
+            printf("Segunda-Feira");
+    }   else if (dia == 3){
+            printf("Terca-Feira");
+    }   else if ( dia == 4){
+            printf("Quarta-Feira");
+    }   else if (dia == 5){
+            printf("Quinta-Feira");
+    }   else if (dia == 6){
+            printf("Sexta-Feira");
+    } 
+    else 
+        printf("Sabado");
+    }
