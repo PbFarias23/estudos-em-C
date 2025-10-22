a2f4f39c8e40f453eac1cad97b5d3e6214ea47bc
nome <email>
2025-10-22 10:32:47 -0300

    Jogo supertrunfo - menu interativo

---
+++ Supertrunfo/supertrunfo - aventureiro.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+
+    char nome1[50] = Rio de Janeiro;
+    char nome2[50] = São Paulo;
+    int populacao1 = 161000000;
+    int populacao2 = 215000000;
+    float area1 = 1200;
+    float area2 = 248219;
+    float pib1 = 1015000000;
+    float pib2 = 3013000000;
+    int pontos1 = 120;
+    int pontos2 = 37;
+    float densidade1 = populacao1 / area1;
+    float densidade2 = populacao2 / area2;
+    int opcao;
+
+
+}
